35185da44702d3cbf44a571e158af3d4ff511b55
arostovstsikov <arostovstsikov@malwarebytes.com>
2020-04-05 14:17:06 +0300

    ci configuration done

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,6 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   // mongoURI: 'mongodb://alexei:2EmZUWvjmhcNxkbr@cluster0-shard-00-00-kejbt.mongodb.net:27017,cluster0-shard-00-01-kejbt.mongodb.net:27017,cluster0-shard-00-02-kejbt.mongodb.net:27017/node-advanced?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
   mongoURI: 'mongodb://testUser:xyz123@localhost:27017/node-advanced',
-  cookieKey: '123123123'
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
